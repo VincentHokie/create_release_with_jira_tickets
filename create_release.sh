fb2e55464213fca88a35770f6bced628f0a13a4e
Vincent Asante <vincenthokie@gmail.com>
2019-02-17 18:07:16 +0300

    add initial script

---
+++ create_release.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+# Setup:
+# 1. Get you a personal GitHub token (https://github.com/settings/tokens/new)
+# 2. Give your token full rights over the "repo" scope
+
+GITHUB_ORGANIZATION=""
+GITHUB_REPOSITORY=""
+GITHUB_ACCESS_TOKEN=""
+
+check_jq() {
+    if ! [ -x "$(command -v jq)" ]; then
+        echo 'Error: jq is not installed.' >&2
+        echo 'Install jq then run this script again.' >&2
+        exit 1
+    fi
+}
+
+check_jira_cli() {
+    if ! [ -x "$(command -v jira)" ]; then
+        echo 'Error: jira cli is not installed.' >&2
+        echo 'Follow this https://docs.jiracli.com to get started with the jira cli.' >&2
+        exit 1
+    fi
+}
+
+check_jira_setup() {
+    if [ ! -f "$HOME/.jira-cl.json" ]; then
+        echo 'Error: jira cli is not set up right.' >&2
+        jira
+        jira config board --set
+    fi
+}
+
+get_ticket_in_latest_release(){
+    # defines the '_all_tickets' variable that is then used in the get_jira_issue_details
+    _current=$(git tag -l --sort=-creatordate | grep -v vTR | grep "^v" | head -1 | tail -1)
+    _previous=$(git tag -l --sort=-creatordate | grep -v vTR | grep "^v" | head -2 | tail -1)
+    echo "Tickets merged between $_previous and $_current"
+    _all_tickets=""
+    for repo in front-end back-end tools # example repos assuming they are siblings of the folder the script is in
+        do 
+            cd ../$repo
+            _tickets=$(git log $_previous..$_current --oneline | grep -v Merge | grep -E -o 'AC-[0-9]{4,}' | sort -u) # we have a pre-commit hook that appends the ticket id to the commits
+            _all_tickets="$_all_tickets $_tickets"
+        done
+    _all_tickets=$(echo $_all_tickets | tr " " "\n" | sort -u)
+}
+
+get_jira_issue_details(){
+    # this is where you'd customize the format of your release body
+    touch .release.txt
+    echo "## Tickets in this release:" >> .release.txt
+    for issue in $@
+        do
+            echo -n "Getting $issue details.."
+            echo -n "* [$issue](https://{ORGANIZATION}.atlassian.net/browse/$issue) - " >> .release.txt # replace ORGANIZATION with your organisations name on jira
+            jira issue $issue | egrep 'Summary' | awk '{out=$6; for(i=7;i<=NF;i++){out=out" "$i}; print out}' >> .release.txt # leave out the color codes in the string
+            echo "Done."
+        done
+}
+
+create_release_doc() {
+    TAG_NAME="$_current"
+    echo "Found tag: ${TAG_NAME}"
+    
+    SUBJECT="Draft ${TAG_NAME} release subject"
+    DESCRIPTION="$(cat .release.txt)"
+
+    echo "Release name: ${SUBJECT}"
+    echo "Release description: ${DESCRIPTION}"
+
+    REQUEST_BODY="$(jq -n --arg tag_name "$TAG_NAME" --arg subject "${SUBJECT}" --arg description "${DESCRIPTION}" \
+    '{"tag_name": $tag_name, "target_commitish": "master", "name": $subject, "body": $description, "draft": true, "prerelease": false} ')"
+
+    echo "Creating release doc.."
+    gh_response=$(curl --data "${REQUEST_BODY}" "https://api.github.com/repos/${GITHUB_ORGANIZATION}/${GITHUB_REPOSITORY}/releases?access_token=${GITHUB_ACCESS_TOKEN}")
+    echo "You can see your draft release at: "
+    echo $gh_response | jq '.html_url'
+
+}
+
+cleanup() {
+    rm -f .release.txt
+}
+
+main() {
+    check_jq
+    check_jira_cli
+    check_jira_setup
+    get_ticket_in_latest_release
+    get_jira_issue_details $_all_tickets
+    create_release_doc
+    cleanup
+}
+
+main "$@"
